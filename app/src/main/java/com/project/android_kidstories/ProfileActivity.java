a2d7d1ab4b75b5ae259b7e768c37295ebf9ebc2c
Chukwu Gideon <chuksgid@gmail.com>
2019-10-10 15:36:39 +0100

    Update ProfileActivity.java

--- app/src/main/java/com/project/android_kidstories/ProfileActivity.java
+++ app/src/main/java/com/project/android_kidstories/ProfileActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.project.android_kidstories.Api.ApiInterface;
 import com.project.android_kidstories.Api.Client;
@@ -13,18 +15,21 @@ import com.project.android_kidstories.Model.User;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private static final String TAG = ProfileActivity.class.getSimpleName();
+    private ApiInterface api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        api = Client.getInstance().create(ApiInterface.class);
     }
 
     private void loadUserProfile(String token){
-        Call<BaseResponse<User>> call = Client.getInstance().create(ApiInterface.class).getUserProfile(token);
+        Call<BaseResponse<User>> call = api.getUserProfile(token);
         call.enqueue(new Callback<BaseResponse<User>>() {
             @Override
             public void onResponse(@NonNull Call<BaseResponse<User>> call, @NonNull Response<BaseResponse<User>> response) {
@@ -39,12 +44,30 @@ public class ProfileActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(@NonNull Call<BaseResponse<User>> call, @NonNull Throwable t) {
-
+                Log.e(TAG, t.getLocalizedMessage());
             }
         });
     }
 
-    private void updateUserProfile(){
+    private void updateUserProfile(String token){
+        String firstname = "";
+        String lastname = "";
+        User user = new  User();
+        user.setFirstName(firstname);
+        user.setLastName(lastname);
+        Call<BaseResponse<User>> call = api.updateUserProfile(token, user);
+        call.enqueue(new Callback<BaseResponse<User>>() {
+            @Override
+            public void onResponse(@NonNull Call<BaseResponse<User>> call, @NonNull Response<BaseResponse<User>> response) {
+                if (response.isSuccessful()){
+                    Toast.makeText(ProfileActivity.this, "User Profile successfully updated", Toast.LENGTH_LONG).show();
+                }
+            }
 
+            @Override
+            public void onFailure(@NonNull Call<BaseResponse<User>> call, @NonNull Throwable t) {
+                Log.e(TAG, t.getLocalizedMessage());
+            }
+        });
     }
 }
